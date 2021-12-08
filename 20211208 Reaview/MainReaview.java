da87d9291b04f009977f677f473301f52c0292af
loveen95 <95738316+loveen95@users.noreply.github.com>
2021-12-08 23:00:25 +0900

    Update MainReaview.java

--- 20211208 Reaview/MainReaview.java
+++ 20211208 Reaview/MainReaview.java
@@ -78,11 +78,105 @@ class Main {
 }
 }
  
+//*toUpperCase* - 모든 문자열을 대문자로 변환
+
+ class Main {
+   public static void main (String[]args) {
+     String a = "Hello World";
+     System.out.println(a.toUpperCase());
+   }
+ }
+ 
+//*Boolean - true 1, false 0
+
+class Main {
+  public static void main (String[] args){
+    int base = 300;
+    int height = 400;
+    boolean isHeavy = height > base;
+
+    if (isHeavy) {
+      System.out.println("더 무겁습니다.");
+    }
+  }
+}
+
+class Main {
+  public static void main(String[]args){
+    String[] weeks = new String[3];
+
+    weeks[0] = "리";
+    weeks[1] = "스";
+    weeks[2] = "트";
+
+  String[] weeks = {"리","스" "트"};
+   System.out.println(weeks[3]);
+
+  }
+}
 
 
+import java.util.ArrayList;
 
-//  -문자열 예-
-//      ex) run, runFast
+public class Main {
+   public static void main(String[] args) {
+
+     ArrayList pitches = new ArrayList();
+
+     pitches.add("123");
+     pitches.add("234");
+     pitches.add("789");
+     pitches.get(1));
+     // get()메서드를 이용하여 특정 인덱스 값을 추출
+
+     System.out.println(pitches.size());
+     // size()메서드를 이용하여 리스트의 개수를 확인
+     System.out.println(pitches.contains("234"));
+     //contains() 메서드를 이용하여 포함 여부 확인
+     System.out.println(pitches.remove("789"));
+    //remove()매서드를 이용하여 해당 객체 삭제 하고 결과를 리턴 
+      System.out.println(pitches.remove(0));
+     //remove()매서드를 이용하여 해당 인덱스의 객체를 삭제
+ }
+ }
+
+ //ArrayList pitches = new //ArrayList();
+
+//pitches 라는 ArrayList에 담을 수 있는 자료형은 String 뿐이다. 
+
+ //ArrayList<String> pitches = //new ArrayList<String>();
+
+
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+
+    //ArrayList aList = new ArrayList();
+    ArrayList<String> aList = new ArrayList<String>();
+
+    aList.add("hello");
+    aList.add("java");
+
+    // String hello = (String)aList.get(0);
+    // String java = (String)aList.get(1);
+    //Casting 강제 형변환
+
+    String hello = aList.get(0);
+    String java = aList.get(1);
+    // 제네릭스를 사용하여 자료형을 선언하면 그 이후로는
+    // 자료형에대한 형변환 casting이 필요 없다. 
+    //이미 자바가 aList에는 반드시 String자료형만 추가
+    //된다는 것을 알고 있다. 제네릭스를 사용하면 형변환에
+    //대한 불필요한 코딩과 잘못된 형변환등의 예외를 방지할 수 있다. 
+
+    System.out.println(hello);
+    System.out.println(java);
+
+  }
+}
+
+
 
 
    
